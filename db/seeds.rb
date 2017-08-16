3753016bc8dd9fc77d49cc2e09e4f2a2250ad145
Rachael Boddy <bodd113@hotmail.co.uk>
2017-08-16 12:20:09 +0100

    added controllers,views and routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Food.spice,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:categories.sample
+    )
+  restaurant.save!
+end
