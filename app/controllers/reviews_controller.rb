5be610e1279b16437d9b0db4c5100ec988b82659
Rachael Boddy <bodd113@hotmail.co.uk>
2017-08-16 12:39:35 +0100

    added form error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
-    redirect_to restaurant_path(@review.restaurant)
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
   end
 
   private
